0732fe4a2f1abc0ecf64c396e890096ba8027e72
kesaite1 <emakkesa@gmail.com>
2025-10-01 22:50:47 +0300

    Hash function's implementation

--- hashfunction.cpp
+++ hashfunction.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
-#include <fstream>
+#include <string>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
-    long input;
+    string input;
+    int sum = 0;
     cout << "Write the input: ";
-    cin >> input;
+    getline(cin, input);
+
+    unsigned char hash[32] = {0};
+
+    for (int i=0; i<input.size(); i++)
+    {
+        hash[i % 32] = (hash[i % 32] + input[i]) % 256;
+
+    }
+
+    cout << "Hash: ";
+    for (int i = 0; i < 32; i++) {
+
+    cout << hex << setw(2) << setfill('0') << (int)hash[i];
+    }
+
+    cout << endl;
+    
 
     return 0;
 }
+
